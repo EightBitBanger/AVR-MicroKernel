4e856a63936456d9df31bb46dc8fc38a87bd728e
SniperChicken32 <66331916+SniperChicken32@users.noreply.github.com>
2021-12-03 22:24:20 -0700

    Process scheduler

--- kernel/scheduler.h
+++ kernel/scheduler.h
@@ -1,5 +1,5 @@
 //
-// Cooperative task scheduler
+// Cooperative scheduler
 #include<avr/interrupt.h>
 
 #define _PROCESS_LIST_SIZE__  24
@@ -11,33 +11,31 @@
 
 struct ProcessScheduler {
 	
-	uint8_t  taskName[_PROCESS_LIST_SIZE__][_PROCESS_NAME_SIZE__];
-	uint8_t  taskType[_PROCESS_LIST_SIZE__];
-	uint16_t taskPriority[_PROCESS_LIST_SIZE__];
-	uint16_t taskCounters[_PROCESS_LIST_SIZE__];
-	void (*task_pointer_table[_PROCESS_LIST_SIZE__])();
+	uint8_t  processName[_PROCESS_LIST_SIZE__][_PROCESS_NAME_SIZE__];
+	uint8_t  processType[_PROCESS_LIST_SIZE__];
+	uint16_t processPriority[_PROCESS_LIST_SIZE__];
+	uint16_t processCounters[_PROCESS_LIST_SIZE__];
+	void (*process_pointer_table[_PROCESS_LIST_SIZE__])();
 	
 	ProcessScheduler() {
 		
-		// Normal counter and waveform
-		// Timer mode with NO prescaler
-		// Enable counter overflow interrupt (TOIEn)
-		TCCR0A  = 0x00;
-		TCCR0B |= (1<<CS10);
-		TIMSK0 |= (1 << TOIE0);
+		// Initiate the interrupt timer
+		TCCR0A  = 0x00;          // Normal counter and waveform
+		TCCR0B |= (1<<CS10);     // Timer mode with NO prescaler
+		TIMSK0 |= (1 << TOIE0);  // Enable counter overflow interrupt (TOIEn)
 		
 		// Setup the task list
 		for (uint8_t i=0; i < _PROCESS_LIST_SIZE__; i++) {
-			for (uint8_t a=0; a < _PROCESS_NAME_SIZE__; a++) taskName[i][a] = 0x20;
-			taskType[i]     = 0x00;
-			taskPriority[i] = 0x00;
-			taskCounters[i] = 0x00;
-			task_pointer_table[i] = (Process&)NULL_f;
+			for (uint8_t a=0; a < _PROCESS_NAME_SIZE__; a++) processName[i][a] = 0x20;
+			processType[i]     = 0x00;
+			processPriority[i] = 0x00;
+			processCounters[i] = 0x00;
+			process_pointer_table[i] = (Process&)NULL_f;
 		}
 		
 	}
 	
-	// Schedule a new process.
+	// Schedule a new process
 	uint8_t createProcess(const char name[], uint8_t name_length, void(*task_ptr)(), uint16_t priority, uint8_t task_type) {
 		
 		if ((priority == 0) || (name_length > _PROCESS_NAME_SIZE__-1)) return 0;
@@ -45,7 +43,7 @@ struct ProcessScheduler {
 		// Find an available slot
 		uint8_t index;
 		for (index=0; index <= _PROCESS_LIST_SIZE__; index++) {
-			if (taskPriority[index] == 0) break; else continue;
+			if (processPriority[index] == 0) break; else continue;
 		}
 		
 		// No free slots
@@ -53,17 +51,17 @@ struct ProcessScheduler {
 		
 		// Launch the new task
 		for (uint8_t a=0; a < name_length-1; a++)
-		taskName[index][a]    = name[a];
+		processName[index][a]    = name[a];
 		
-		taskType[index]           = task_type;
-		taskPriority[index]       = priority;
-		taskCounters[index]       = 0;
-		task_pointer_table[index] = task_ptr;
+		processType[index]           = task_type;
+		processPriority[index]       = priority;
+		processCounters[index]       = 0;
+		process_pointer_table[index] = task_ptr;
 		
 		return 1;
 	}
 	
-	// Get a process pointer by a process name.
+	// Get a process pointer by a process name
 	Process& getProcess(const char process_name[], uint8_t name_length) {
 		
 		if (name_length > _PROCESS_NAME_SIZE__-1) return (Process&)NULL_f;
@@ -71,16 +69,16 @@ struct ProcessScheduler {
 		// Function look up
 		for (uint8_t i=0; i < _PROCESS_LIST_SIZE__; i++) {
 			
-			if (taskName[i][0] == 0x20) continue;
+			if (processName[i][0] == 0x20) continue;
 			
 			uint8_t count=1;
 			for (uint8_t a=0; a < name_length; a++) {
-				char nameChar = taskName[i][a];
+				char nameChar = processName[i][a];
 				if (nameChar == process_name[a]) count++; else break;
 			}
 			
 			if (count >= name_length)
-			return task_pointer_table[i];
+			return process_pointer_table[i];
 			
 		}
 		
@@ -95,15 +93,15 @@ struct ProcessScheduler {
 		// Find the task
 		uint8_t index;
 		for (index=0; index <= _PROCESS_LIST_SIZE__; index++)
-		if (task_pointer_table[index] == process) break; else continue;
+		if (process_pointer_table[index] == process) break; else continue;
 		
 		// Check no free slots
 		if (index == _PROCESS_LIST_SIZE__) return 0;
 		
 		// Kill the task
-		taskPriority[index] = 0;
-		taskCounters[index] = 0;
-		task_pointer_table[index] = (Process&)NULL_f;
+		processPriority[index] = 0;
+		processCounters[index] = 0;
+		process_pointer_table[index] = (Process&)NULL_f;
 		
 		return 1;
 	}
@@ -122,31 +120,29 @@ ISR (TIMER0_OVF_vect) {
 	
 	for (uint8_t index=0; index < _PROCESS_LIST_SIZE__; index++) {
 		
-		// Check task
-		if (scheduler.taskPriority[index] == 0) continue;
+		if (scheduler.processPriority[index] == 0) continue;
 		
-		// Increment the task counter
-		scheduler.taskCounters[index]++;
+		// Check per process counter
+		scheduler.processCounters[index]++;
+		if (scheduler.processCounters[index] < scheduler.processPriority[index]) {continue;} else {
+			// Run the process
+			scheduler.processCounters[index]=0;
+			scheduler.process_pointer_table[index]();
+		}
 		
-		// Continue if the task should NOT be executed
-		if (scheduler.taskCounters[index] < scheduler.taskPriority[index]) continue;
-		
-		// Reset the counter and call the task
-		scheduler.taskCounters[index] = 0;
-		scheduler.task_pointer_table[index]();
-		
-		// Check task volatile
-		if (scheduler.taskType[index] == _PROCESS_TYPE_VOLATILE__) {
-			// Remove task
-			for (uint8_t a=0; a < _PROCESS_NAME_SIZE__; a++) scheduler.taskName[index][a] = 0x20;
-			scheduler.taskType[index] = 0;
-			scheduler.taskPriority[index] = 0;
-			scheduler.taskCounters[index] = 0;
-			scheduler.task_pointer_table[index] = (Process&)NULL_f;
+		// Check volatility
+		if (scheduler.processType[index] == _PROCESS_TYPE_VOLATILE__) {
+			// Remove process
+			for (uint8_t a=0; a < _PROCESS_NAME_SIZE__; a++) scheduler.processName[index][a] = 0x20;
+			scheduler.processType[index] = 0;
+			scheduler.processPriority[index] = 0;
+			scheduler.processCounters[index] = 0;
+			scheduler.process_pointer_table[index] = (Process&)NULL_f;
 		}
 		
 	}
 	
+	return;
 }
 
 
