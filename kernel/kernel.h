cb1c3e728ae9d8a46f26970378899d211465ad73
SniperChicken32 <66331916+SniperChicken32@users.noreply.github.com>
2022-05-22 07:43:28 -0600

    Add files via upload

---
+++ kernel/kernel.h
@@ -0,0 +1,34 @@
+//
+// AVR Micro Kernel
+
+#ifndef __KERNEL_MAIN__
+#define __KERNEL_MAIN__
+
+// Default device names
+const char _DISPLAY_CONSOLE__[]   = "display";
+const char _COMMAND_CONSOLE__[]   = "console";
+const char _KEYBOARD_INPUT__[]    = "keyboard";
+const char _EXTENDED_MEMORY__[]   = "ExMem";
+const char _HARD_STORAGE__[]      = "Storage";
+
+
+#include "string_const.h"
+
+#define nop  asm("nop")
+
+#include "tables\drivers.h"    // Device drivers
+#include "tables\modules.h"    // Application modules
+#include "tables\scheduler.h"  // Task scheduler
+#include "bus.h"               // System bus interface
+
+
+// Standard includes
+#include "std\pointers.h"
+#include "std\cstring.h"
+
+#include "syscalls.h"          // System call wrap functions
+
+
+
+#endif
+
---
+++ kernel/syscalls.h
@@ -0,0 +1,91 @@
+// System call routing functions
+
+// Allocation
+uint32_t stack_alloc(uint32_t size);
+uint32_t stack_free(uint32_t size);
+
+// File system
+void fs_seek(uint32_t address); // Set seek location pointer
+void fs_write(uint8_t byte);    // Write to location auto-incrementing the seek pointer
+void fs_read(uint8_t& byte);    // Read from location auto-incrementing the seek pointer
+
+
+
+
+//
+// Allocation
+
+uint32_t stack_alloc(uint32_t size) {
+	
+	WrappedPointer pointer(size);
+	DriverEntryPoint driverEntry;
+	
+	if (getFuncAddress(_EXTENDED_MEMORY__, sizeof(_EXTENDED_MEMORY__), driverEntry) == 0) return nullptr;
+	
+	// Set requested size
+	callExtern(driverEntry, 0x0a, pointer.byte_t[0], pointer.byte_t[1], pointer.byte_t[2], pointer.byte_t[3]);
+	// Request the memory
+	callExtern(driverEntry, 0x00);
+	// Get return result
+	callExtern(driverEntry, 0x0c, pointer.byte_t[0], pointer.byte_t[1], pointer.byte_t[2], pointer.byte_t[3]);
+	
+	return pointer.address;
+}
+
+uint32_t stack_free(uint32_t size) {
+	
+	WrappedPointer pointer(size);
+	DriverEntryPoint driverEntry;
+	
+	if (getFuncAddress(_EXTENDED_MEMORY__, sizeof(_EXTENDED_MEMORY__), driverEntry) == 0) return nullptr;
+	
+	// Set requested size
+	callExtern(driverEntry, 0x0a, pointer.byte_t[0], pointer.byte_t[1], pointer.byte_t[2], pointer.byte_t[3]);
+	// Free the memory
+	callExtern(driverEntry, 0x01);
+	// Get return result
+	callExtern(driverEntry, 0x0c, pointer.byte_t[0], pointer.byte_t[1], pointer.byte_t[2], pointer.byte_t[3]);
+	
+	return pointer.address;
+}
+
+
+//
+// File system
+
+void fs_write(uint8_t byte) {
+	
+	DriverEntryPoint storageDriverPtr;
+	
+	// Link to system storage
+	if (getFuncAddress(_HARD_STORAGE__, sizeof(_HARD_STORAGE__), storageDriverPtr) == nullptr) return;
+	
+	callExtern(storageDriverPtr, 0x03, byte);
+	
+}
+
+void fs_read(uint8_t& byte) {
+	
+	DriverEntryPoint storageDriverPtr;
+	
+	// Link to system storage
+	if (getFuncAddress(_HARD_STORAGE__, sizeof(_HARD_STORAGE__), storageDriverPtr) == nullptr) return;
+	
+	callExtern(storageDriverPtr, 0x04, byte);
+	
+}
+
+void fs_seek(uint32_t address) {
+	
+	WrappedPointer pointer;
+	pointer.address = address;
+	DriverEntryPoint storageDriverPtr;
+	
+	// Link to system storage
+	if (getFuncAddress(_HARD_STORAGE__, sizeof(_HARD_STORAGE__), storageDriverPtr) == nullptr) return;
+	
+	callExtern(storageDriverPtr, 0x00, pointer.byte_t[0], pointer.byte_t[1], pointer.byte_t[2], pointer.byte_t[3]);
+	
+}
+
+
