e86ef7c5726f6cf2f5edb001876fa920e487bde5
SniperChicken32 <66331916+SniperChicken32@users.noreply.github.com>
2022-05-31 21:12:11 -0600

    Add files via upload

--- modules/functions/port.h
+++ modules/functions/port.h
@@ -3,35 +3,33 @@
 
 void __io_control_(void);
 
-struct ModuleLoaderIOport {
-	ModuleLoaderIOport() {
+struct ModuleLoaderPort {
+	
+	uint32_t address;
+	
+	ModuleLoaderPort() {
+		address=0x00000;
 		load_module("port",  5, __io_control_);
 	}
-}static moduleLoaderIOport;
+}static moduleLoaderPort;
 
 void executable_task(void);
-
-//
-// Entry point
-
-
-
-
 
 
 void __io_control_(void) {
 	
 	Bus device_bus;
+	
 	device_bus.waitstate_read  = 0;
 	device_bus.waitstate_write = 0;
 	
 	// Port selection
-	if ((console.keyboard_string[5] == 's') && (console.keyboard_string[6] == 'e') && (console.keyboard_string[7] == 't')) {
-		
-		if ((console.keyboard_string[9] == '1') == 1) {
-			
-		}
-		
+	if ((console.keyboard_string[5] == 'a') == 1) {
+		moduleLoaderPort.address = 0xc0000;
+		return;
+	}
+	if ((console.keyboard_string[5] == 'b') == 1) {
+		moduleLoaderPort.address = 0xd0000;
 		return;
 	}
 	
@@ -45,11 +43,10 @@ void __io_control_(void) {
 		
 		value = string_get_hex(string);
 		
-		bus_write_byte(device_bus, 0xd0000, value);
+		bus_write_byte(device_bus, moduleLoaderPort.address, value);
 		
 		return;
 	}
-	
 	
 	// Bit field value
 	uint8_t byte = 0x00;
@@ -63,7 +60,7 @@ void __io_control_(void) {
 	if (console.keyboard_string[11] == 0x31) byte |= 0b01000000;
 	if (console.keyboard_string[12] == 0x31) byte |= 0b10000000;
 	
-	bus_write_byte(device_bus, 0xd0000, byte);
+	bus_write_byte(device_bus, moduleLoaderPort.address, byte);
 	
 	return;
 }
