20a4b1199cef33fe53a5e0c1994e47d90e941e14
SniperChicken32 <66331916+SniperChicken32@users.noreply.github.com>
2022-08-25 21:17:59 -0600

    Add files via upload

--- modules/fs/cd.h
+++ modules/fs/cd.h
@@ -20,6 +20,35 @@ void command_cd(uint8_t, uint8_t&, uint8_t&, uint8_t&, uint8_t&) {
 	if (storageDevice == 0) return;
 	
 	uint8_t param0 = console.keyboard_string[sizeof(__MODULE_NAME_)];
+	uint8_t param1 = console.keyboard_string[sizeof(__MODULE_NAME_) + 2];
+	uint8_t param2 = console.keyboard_string[sizeof(__MODULE_NAME_) + 3];
+	
+	if (param0 == 's') {
+		
+		char hex_string[2] = {'0', '0'};
+		uint8_t char_b = console.keyboard_string[sizeof(__MODULE_NAME_) + 2];
+		uint8_t char_a = console.keyboard_string[sizeof(__MODULE_NAME_) + 3];
+		
+		if ((((char_a >= '0') & (char_a <= '9')) | ((char_a >= 'a') & (char_a <= 'f'))) |
+		(((char_b >= '0') & (char_b <= '9')) | ((char_b >= 'a') & (char_b <= 'f')))) {
+			
+			hex_string[0] = char_a;
+			hex_string[1] = char_b;
+			
+			moduleLoaderMk.file_size = string_get_hex_char(hex_string);
+			
+			console.printHex(moduleLoaderMk.file_size);
+			console.printLn();
+			
+			if (hex_string[0] == ' ') {
+				hex_string[0] = hex_string[1];
+				hex_string[1] = '0';
+			}
+			
+		}
+		return;
+	}
+	
 	
 	if ((param0 >= 'a') & (param0 <= 'e')) 
 		console.promptString[0] = (param0 - 0x20);
--- modules/fs/mk.h
+++ modules/fs/mk.h
@@ -11,7 +11,7 @@ struct ModuleLoaderMk {
 	
 	ModuleLoaderMk() {
 		
-		file_size       = 1;
+		file_size = 1;
 		
 		load_device(__MODULE_NAME_, sizeof(__MODULE_NAME_), (Module)command_mk, DEVICE_TYPE_MODULE);
 	}
@@ -32,11 +32,14 @@ void command_mk(uint8_t, uint8_t&, uint8_t&, uint8_t&, uint8_t&) {
 	char byte;
 	
 	uint32_t current_device = 0x30000 + (0x10000 * (console.promptString[0] - 'A' + 1));
-	uint32_t stride         = 32;
-	uint32_t device_start   = current_device;
-	uint32_t device_end     = current_device + (stride * 256);   // 8k
 	
-	uint8_t page_counter=0;
+	uint32_t stride              = 32;
+	uint32_t device_start        = current_device;
+	uint32_t device_end          = current_device + (stride * 256);   // 8k
+	
+	uint32_t total_sectors       = device_end / stride;
+	uint32_t file_sector_size    = moduleLoaderMk.file_size / stride;
+	uint32_t count_free_sectors  = 0;
 	
 	// Get filename parameter
 	char filename[11] = "          ";
@@ -55,23 +58,33 @@ void command_mk(uint8_t, uint8_t&, uint8_t&, uint8_t&, uint8_t&) {
 		// Check sector is empty
 		if (byte != 0x00) continue;
 		
-		// Check next bytes for file size
-		uint16_t count_free_sectors=0;
-		for (uint32_t a=device_start; a < device_end; a += stride) {
+		// Check next bytes for requested file size
+		for (uint32_t a=1; a < total_sectors; a++) {
 			
-			pointer.address = i;
+			pointer.address = i + (a * stride);
 			call_extern(storageDevice, DEVICE_CALL_ADDRESS, pointer.byte_t[0], pointer.byte_t[1], pointer.byte_t[2], pointer.byte_t[3]);
 			call_extern(storageDevice, 0x00, (uint8_t&)byte);
 			
-			if (byte != 0x00) continue;
+			if (byte == 0x00) {
+				count_free_sectors++;
+				
+				if (count_free_sectors >= file_sector_size) {
+					count_free_sectors = file_sector_size;
+					break;
+				}
+				
+				continue;
+			}
 			
-			
-			
-			
+			break;
 		}
 		
+		// File cannot fit into this empty space, try again
+		if (file_sector_size != count_free_sectors) 
+			continue;
 		
-		// Mark sector as used
+		
+		// File size is available, mark sector as taken
 		byte = 0x55; // File start byte
 		pointer.address = i;
 		call_extern(storageDevice, DEVICE_CALL_ADDRESS, pointer.byte_t[0], pointer.byte_t[1], pointer.byte_t[2], pointer.byte_t[3]);
@@ -99,23 +112,24 @@ void command_mk(uint8_t, uint8_t&, uint8_t&, uint8_t&, uint8_t&) {
 			_delay_ms(5);
 		}
 		
-		uint32_t number_of_sectors = (moduleLoaderMk.file_size / 32) + 2;
+		uint32_t number_of_sectors = (moduleLoaderMk.file_size / stride) + 1;
+		if (number_of_sectors <= 2) number_of_sectors = 2;
 		
-		// Mark following sectors
+		// Mark following sectors as taken
 		byte = 0xff;
 		for (uint32_t a=1; a < number_of_sectors; a++) {
 			
-			if (a == number_of_sectors-1) {byte = 0xaa;
-				
-				pointer.address = i + (a * stride);
-				call_extern(storageDevice, DEVICE_CALL_ADDRESS, pointer.byte_t[0], pointer.byte_t[1], pointer.byte_t[2], pointer.byte_t[3]);
-				call_extern(storageDevice, 0x01, (uint8_t&)byte);
-				_delay_ms(5);
-			}
+			if (a == (number_of_sectors-1)) 
+				byte = 0xaa;
 			
-			return;
+			pointer.address = i + (a * stride);
+			call_extern(storageDevice, DEVICE_CALL_ADDRESS, pointer.byte_t[0], pointer.byte_t[1], pointer.byte_t[2], pointer.byte_t[3]);
+			call_extern(storageDevice, 0x01, (uint8_t&)byte);
+			_delay_ms(5);
+			
 		}
 		
+		return;
 	}
 	
 	return;
