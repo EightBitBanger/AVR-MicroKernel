e96aa54fe7bb49ba5896e8c58c4ff3fd39cd78d6
RetroBytes32 <66331916+RetroBytes32@users.noreply.github.com>
2024-05-13 20:56:56 -0600

    Delete kernel/kernel directory

--- kernel/kernel/commands/device.h
+++
@@ -1,7 +0,0 @@
-#ifndef _DEVICE_LIST__
-#define _DEVICE_LIST__
-
-void registerCommandDevice(void);
-
-
-#endif
--- kernel/kernel/commands/edit.h
+++
@@ -1,6 +0,0 @@
-#ifndef _CONSOLE_COMMAND_EDITOR__
-#define _CONSOLE_COMMAND_EDITOR__
-
-void registerCommandEDIT(void);
-
-#endif
