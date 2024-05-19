d3b3067c66fa4eef234c65828f848e8e383c0e2a
RetroBytes32 <66331916+RetroBytes32@users.noreply.github.com>
2024-05-19 15:30:44 -0600

    Add files via upload

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,11 +2,32 @@
 
 void _ISR_hardware_service_routine(void) {
     
-    printc("fuck", 5);
+    struct Bus bus;
+    bus.read_waitstate  = 2;
+    
+    uint8_t interruptVector = 0;
+    
+    bus_read_io(&bus, 0x00000, &interruptVector);
+    
+    if (interruptVector == 1)   printc("1", 4);
+    if (interruptVector == 2)   printc("2", 4);
+    if (interruptVector == 4)   printc("3", 4);
+    if (interruptVector == 8)   printc("4", 4);
+    if (interruptVector == 16)  printc("5", 4);
+    if (interruptVector == 32)  printc("6", 4);
+    if (interruptVector == 64)  printc("7", 4);
+    if (interruptVector == 128) printc("8", 4);
+    
+    uint8_t msgInt[] = "INT";
+    
+    printLn();
+    
+    print(msgInt, sizeof(msgInt));
+    
     printLn();
     
     while(1) {
-        continue;
+        
     }
     
     return;
