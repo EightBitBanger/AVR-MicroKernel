d208c213adf99f749f5af438b9a1c9206214dcf7
RetroBytes32 <66331916+RetroBytes32@users.noreply.github.com>
2024-05-24 21:57:07 -0600

    Add files via upload

--- kernel/filesystem/fsFileIO.c
+++ kernel/filesystem/fsFileIO.c
@@ -6,15 +6,21 @@
 #include <kernel/filesystem/fs.h>
 
 uint32_t fileBeginAddress = 0;
+uint32_t fileSize = 0;
 
 
 uint8_t fsFileOpen(uint8_t* name, uint8_t nameLength) {
     
     fileBeginAddress = fsFileExists(name, nameLength);
     
-    if (fileBeginAddress != 0)
+    if (fileBeginAddress != 0) {
+        
+        fileSize = fsGetFileSize(name, nameLength);
+        
         return 1;
+    }
     
+    fileSize = 0;
     return 0;
 }
 
@@ -22,6 +28,7 @@ uint8_t fsFileClose(uint8_t index) {
     
     if (fileBeginAddress != 0) {
         fileBeginAddress = 0;
+        fileSize = 0;
         return 1;
     }
     
@@ -39,6 +46,9 @@ uint8_t fsFileWrite(uint8_t index, uint8_t* buffer, uint32_t length) {
     
     uint32_t sector = 0;
     uint32_t sectorCounter = 0;
+    
+    //if (length > fileSize) 
+    //    return 0;
     
     for (uint32_t i=0; i < length; i++) {
         
@@ -70,6 +80,9 @@ uint8_t fsFileRead(uint8_t index, uint8_t* buffer, uint32_t length) {
     
     uint32_t sector = 0;
     uint32_t sectorCounter = 0;
+    
+    if (length > fileSize) 
+        return 0;
     
     for (uint32_t i=0; i < length; i++) {
         
