903b57a9064a837972d317f3983b1fbfd91bcd56
RetroBytes32 <66331916+RetroBytes32@users.noreply.github.com>
2024-05-26 18:36:45 -0600

               ​‍‌‎‏‪‫‬‭‮

---
+++ kernel/kernel.c
@@ -0,0 +1,86 @@
+#include <kernel/kernel.h>
+#include <kernel/delay.h>
+
+#define INT_VECT_0  254
+#define INT_VECT_1  253
+#define INT_VECT_2  251
+#define INT_VECT_3  247
+#define INT_VECT_4  239
+#define INT_VECT_5  223
+#define INT_VECT_6  191
+#define INT_VECT_7  127
+
+#define INTERRUPT_VECTOR_TABLE_SIZE  8
+void defaultLandingFunction(uint8_t index) {}
+
+void (*interrupt_vector_table[INTERRUPT_VECTOR_TABLE_SIZE])(uint8_t);
+
+void kInit(void) {
+    
+    uint8_t deviceIndex=0;
+    
+    // Enumerate available hardware devices
+    for (uint8_t d=1; d <= NUMBER_OF_PERIPHERALS; d++) {
+        
+        fsSetCurrentDevice( d );
+        
+        if (fsCheckDeviceReady() == 0) 
+            continue;
+        
+        fsSetCurrentDevice( 0xff );
+        
+        // Set the root directory
+        uint8_t filename[] = "storage0";
+        filename[7] = '0' + (deviceIndex);
+        deviceIndex++;
+        
+        fsFileCreate(filename, sizeof(filename)-1, 80, ' ');
+        
+        uint8_t index;
+        index = fsFileOpen(filename, sizeof(filename)-1);
+        uint8_t lineA[] = "ld=drvsys\n\0";
+        
+        fsFileWrite(index, lineA, sizeof(lineA));
+        
+        fsFileClose(index);
+        
+        continue;
+    }
+    
+    fsSetCurrentDevice( 0xff );
+    
+    return;
+}
+
+void kernelVectorTableInit(void) {
+    
+    for (uint8_t i=0; i < INTERRUPT_VECTOR_TABLE_SIZE; i++) {
+        
+        interrupt_vector_table[i] = defaultLandingFunction;
+        
+    }
+    
+    return;
+}
+
+void _ISR_hardware_service_routine(void) {
+    
+    struct Bus bus;
+    bus.read_waitstate  = 4;
+    
+    uint8_t iVect = 0;
+    
+    bus_read_io(&bus, 0x00000, &iVect);
+    
+    if (iVect == INT_VECT_0) {interrupt_vector_table[0](0);}
+    if (iVect == INT_VECT_1) {interrupt_vector_table[1](1);}
+    if (iVect == INT_VECT_2) {interrupt_vector_table[2](2);}
+    if (iVect == INT_VECT_3) {interrupt_vector_table[3](3);}
+    if (iVect == INT_VECT_4) {interrupt_vector_table[4](4);}
+    if (iVect == INT_VECT_5) {interrupt_vector_table[5](5);}
+    if (iVect == INT_VECT_6) {interrupt_vector_table[6](6);}
+    if (iVect == INT_VECT_7) {interrupt_vector_table[7](7);}
+    
+    return;
+}
+
---
+++ kernel/main.c
@@ -0,0 +1,175 @@
+#include <kernel/main.h>
+
+int main(void) {
+    
+    // Zero the system bus
+    bus_control_zero();
+    bus_address_zero();
+    
+    // Allow board some time to stabilize
+    _delay_ms(800);
+    
+    // Device drivers
+	initiateDisplayDriver();      // 20x4 LCD Display
+	initiatePS2Driver();          // PS/2 Port
+	initiateNetworkDriver();      // UART Network Card
+	initiateSpeakerDriver();      // On-Board speaker
+	
+	// Initiate kernel sub systems
+	InitiateDeviceTable();
+    
+    consoleInit();                // Command console shell
+    
+    //
+    // Allocate external memory
+    //
+    
+    AllocateExternalMemory();
+    
+    //
+    // Speaker beep
+    //
+    
+#ifdef BOARD_RETRO_AVR_X4_REV1
+    
+    uint16_t duration  = 120;
+    uint16_t frequency = 20000;
+    
+    spkBeep(duration, frequency);
+    
+#endif
+    
+#ifdef NETWORK_APPLICATION_PACKET_ROUTER
+    
+    InitiateRouter();
+    
+#endif
+    
+#ifdef NETWORK_APPLICATION_SERVER
+    
+    InitiateServer();
+    
+#endif
+    
+#ifndef DONT_INCLUDE_CONSOLE_COMMANDS
+    
+  #ifdef INCLUDE_KERNEL_APPLICATIONS
+    
+    //registerCommandDevice();
+    //registerCommandCAP();
+    //registerCommandList();
+    //registerCommandTASK();
+    
+    //registerCommandEDIT();
+    //registerCommandAssembly();
+    
+    //registerCommandCLS();
+    
+  #endif
+    
+  #ifdef INCLUDE_NETWORK_APPLICATIONS
+    
+    //registerCommandNet();
+    
+  #endif
+    
+  #ifdef INCLUDE_FILE_SYSTEM_APPLICATIONS
+    
+    registerCommandLS();
+    //registerCommandCOPY();
+    registerCommandCD();
+    
+    //registerCommandMK();
+    //registerCommandRM();
+    //registerCommandRN();
+    registerCommandMKDIR();
+    
+    //registerCommandATTRIB();
+    //registerCommandRepair();
+    //registerCommandFormat();
+    
+  #endif
+    
+#endif
+    
+    //
+    // Boot the kernel
+    // 
+    
+    kernelVectorTableInit();      // Hardware interrupt vector table
+    schedulerInit();              // Scheduler sub system
+    fsInit();                     // File system
+    ntInit();                     // Network support
+    kInit();                      // Setup the kernel environment
+    
+    ConsoleSetBlinkRate( CURSOR_BLINK_RATE );
+    
+    // Version
+    uint8_t kernelHelloWorldString[] = "kernel";
+    print(kernelHelloWorldString, sizeof(kernelHelloWorldString));
+    
+    uint8_t versionMajor = _KERNEL_VERSION_MAJOR__;
+    uint8_t versionMinor = _KERNEL_VERSION_MINOR__;
+    uint8_t versionPatch = _KERNEL_VERSION_PATCH__;
+    
+    printSpace(1);
+    printChar('v');
+    printInt(versionMajor);
+    printChar('.');
+    printInt(versionMinor);
+    printChar('.');
+    printInt(versionPatch);
+    printLn();
+    
+    
+    
+    //
+    // Start in the root directory
+    
+    fsSetCurrentDevice( 0xff );
+    
+    uint8_t prompt[] = "/>";
+    ConsoleSetPrompt( prompt, sizeof(prompt) );
+    
+    // Drop the initial command prompt
+    printPrompt();
+    
+    //
+    // Launch system tasks
+    
+    // Command console
+    uint8_t taskname[] = "command";
+    TaskCreate(taskname, sizeof(taskname), consoleRunShell, TASK_PRIORITY_REALTIME, TASK_TYPE_SERVICE);
+    
+    
+    //
+    // Hardware/software interrupt handling
+    
+    // Enable hardware interrupts
+    //  Trigger on the HIGH to LOW transition of PIN2
+    InterruptStartHardware();
+    
+    SetHardwareInterruptServiceA( _ISR_hardware_service_routine );
+    
+    // Prepare the scheduler and its 
+    // associated hardware interrupts
+    SchedulerStart();
+    
+    InterruptStartScheduler();
+    InterruptStartTimeCounter();
+    
+    while(1) {
+        
+        __asm__("nop");
+        
+        continue;
+    }
+    
+    InterruptStopTimerCounter();
+    InterruptStopScheduler();
+    InterruptStopHardware();
+    
+    SchedulerStop();
+    
+    return 0;
+}
