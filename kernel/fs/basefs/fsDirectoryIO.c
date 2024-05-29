b3b9c548e35d98d7e5d6348f4dd6725e4760c45c
RetroBytes32 <66331916+RetroBytes32@users.noreply.github.com>
2024-05-28 22:49:01 -0600

               ​‍‌‎‏‪‫‬‭‮

--- kernel/fs/basefs/fsDirectoryIO.c
+++ kernel/fs/basefs/fsDirectoryIO.c
@@ -1,10 +1,47 @@
 #include <kernel/kernel.h>
 
-
-uint32_t fsDirectorySetNumberOfFiles(uint8_t* name, uint8_t nameLength, uint32_t numberOfFiles) {
+uint8_t fsDirectorySetNumberOfFiles(uint8_t* name, uint8_t nameLength, uint32_t numberOfFiles) {
+    
+    uint32_t fileAddress = fsFileExists(name, nameLength);
+    
+    if (fileAddress == 0) 
+        return 0;
+    
+    struct Bus bus;
+    bus.read_waitstate  = 5;
+    
+    // Read file size
+    union Pointer directorySize;
+    
+    directorySize.address = numberOfFiles;
+    
+    for (uint8_t i=0; i < 4; i++) 
+        fs_write_byte( &bus, fileAddress + OFFSET_DIRECTORY_SIZE + i, directorySize.byte_t[i]);
     
     return 1;
 }
+
+uint32_t fsDirectoryGetNumberOfFiles(uint8_t* name, uint8_t nameLength) {
+    
+    uint32_t fileAddress = fsFileExists(name, nameLength);
+    
+    if (fileAddress == 0) 
+        return 0;
+    
+    struct Bus bus;
+    bus.read_waitstate  = 5;
+    
+    // Read file size
+    union Pointer directorySize;
+    
+    for (uint8_t i=0; i < 4; i++) 
+        fs_read_byte( &bus, fileAddress + OFFSET_DIRECTORY_SIZE + i, &directorySize.byte_t[i]);
+    
+    return directorySize.address;
+}
+
+
+
 
 /*
 uint32_t fileBeginAddress = 0;
