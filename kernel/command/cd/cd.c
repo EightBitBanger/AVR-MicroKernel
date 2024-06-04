b22cafc784e8e875612e3153530f95c24642c7e4
RetroBytes32 <66331916+RetroBytes32@users.noreply.github.com>
2024-06-03 19:47:21 -0600

               ​‍‌‎‏‪‫‬‭‮

--- kernel/command/cd/cd.c
+++ kernel/command/cd/cd.c
@@ -13,59 +13,111 @@ struct DirectoryName {
 
 struct DirectoryName directoryStack[8];
 
+extern uint8_t fs_directory_stack_ptr;
+
+
 void functionCD(uint8_t* param, uint8_t param_length) {
     
     uint8_t msgDeviceError[]        = "Device not ready";
     uint8_t msgDirectoryNotFound[]  = "Directory not found";
     uint8_t msgNotDirectory[]       = "Not a directory";
     
+    
     //
-    // Change device letter
+    // Change Device
     //
     
     // The letter represents the hardware address 
     // offset pointing to the device on the system bus
     
-    /*
+    uint8_t deviceLetter = param[0];
+    uppercase(&deviceLetter);
     
-    if ((param[0] >= 'a') & (param[0] <= 'z') & (param[1] == ':')) {
+    if ((deviceLetter >= 'A') & (deviceLetter <= 'Z') & (param[1] == ':')) {
         
-        uint8_t deviceLetter = param[0] - 'a';
-        
-        fsSetCurrentDevice( deviceLetter );
-        uppercase(&param[0]);
+        fsSetDeviceLetter( deviceLetter );
+        fsSetRootDirectory( deviceLetter );
         
         uint8_t consolePrompt[2];
-        consolePrompt[0] = param[0];
+        consolePrompt[0] = deviceLetter;
         consolePrompt[1] = '>';
         
         ConsoleSetPrompt(consolePrompt, 3);
         
-        fsSetRootDirectory(param[0]);
-        
         fsClearWorkingDirectory();
+        fs_directory_stack_ptr = 0;
         
         return;
     }
     
     
-    // Check internal storage switch
-    if ((param[0] == '/') & (param[1] == ':')) {
+    //
+    // Change Directory
+    //
+    
+    if ((deviceLetter >= 'A') & (deviceLetter <= 'Z')) {
         
-        fsSetCurrentDevice( 0xff );
-        fsSetRootDirectory('/');
+        if (fsFileExists(param, param_length-1) == 0) {
+            
+            print(msgDirectoryNotFound, sizeof(msgDirectoryNotFound));
+            printLn();
+            
+            return;
+        }
         
-        uint8_t PromptBase[] = "/>";
-        ConsoleSetPrompt(PromptBase, sizeof(PromptBase));
+        struct FSAttribute attribute;
+        fsGetFileAttributes(param, param_length-1, &attribute);
         
-        fsClearWorkingDirectory();
+        // Check is a directory
+        if (attribute.type != 'd') {
+            
+            print(msgNotDirectory, sizeof(msgNotDirectory));
+            printLn();
+            
+            return;
+        }
         
-        fsSetDirectoryStack(0);
+        
+        // Check device
+        
+        if (fsCheckDeviceReady() == 0) {
+            
+            print(msgDeviceError, sizeof(msgDeviceError));
+            printLn();
+            
+            return;
+        }
+        
+        fsSetWorkingDirectory(param, param_length);
+        
+        fs_directory_stack_ptr++;
+        
+        // Add the directory to the directory stack
+        for (uint8_t n=0; n < 10; n++) 
+            directoryStack[fs_directory_stack_ptr].name[n] = ' ';
+        
+        // Prompt for device letters
+        
+        for (uint8_t n=0; n < param_length - 1; n++) 
+            directoryStack[fs_directory_stack_ptr].name[n] = param[n];
+        
+        uint8_t PromptDir[20];
+        
+        // Device letter
+        for (uint8_t i=0; i < param_length + 1; i++) 
+            PromptDir[i + 2] = param[i];
+        
+        PromptDir[param_length + 1] = '>';
+        
+        PromptDir[0] = fsGetRootDirectory();
+        PromptDir[1] = '/';
+        
+        ConsoleSetPrompt(PromptDir, param_length + 3);
         
         return;
     }
     
-    */
+    
     
     //
     // Drop down the parent directory
@@ -73,24 +125,20 @@ void functionCD(uint8_t* param, uint8_t param_length) {
     
     if ((param[0] == '.') & (param[1] == '.') & (param[2] == ' ')) {
         
-        uint8_t directoryStackPtr = fsGetDirectoryStack();
-        
-        if (directoryStackPtr == 0) 
-            return;
-        
-        if (directoryStackPtr > 1) {
+        if (fs_directory_stack_ptr > 0) {
             
-            // Drop to parent directory
-            directoryStackPtr--;
+            printc("AAA", 3);
+            printLn();
             
-            fsSetDirectoryStack(directoryStackPtr);
+            // Drop to the parent directory
+            fs_directory_stack_ptr--;
             
             uint8_t filename[20];
             uint8_t filenameLength = 0;
             
             for (uint8_t n=0; n < 10; n++) {
                 
-                filename[n] = directoryStack[directoryStackPtr].name[n];
+                filename[n] = directoryStack[fs_directory_stack_ptr].name[n];
                 
                 filenameLength = n + 1;
                 
@@ -98,36 +146,37 @@ void functionCD(uint8_t* param, uint8_t param_length) {
                     break;
             }
             
+            // Set the parent directory
             fsSetWorkingDirectory(filename, filenameLength-1);
             
             uint8_t PromptDir[20];
             
-            if (fsGetRootDirectory() == '/') {
+            for (uint8_t n=0; n < 10; n++) 
+                directoryStack[fs_directory_stack_ptr].name[n] = ' ';
+            
+            // Prompt for device letters
+            
+            for (uint8_t n=0; n < filenameLength - 1; n++) {
+                directoryStack[fs_directory_stack_ptr].name[n] = filename[n];
                 
-                for (uint8_t n=0; n < 10; n++) 
-                    directoryStack[directoryStackPtr].name[n] = ' ';
-                
-                for (uint8_t n=0; n < filenameLength - 1; n++) {
-                    directoryStack[directoryStackPtr].name[n] = filename[n];
-                    
-                    PromptDir[n + 1] = filename[n];
-                }
-                
-                PromptDir[filenameLength] = '>';
-                
-                PromptDir[0] = '/';
-                
-                ConsoleSetPrompt(PromptDir, filenameLength + 2);
-                
+                PromptDir[n + 2] = filename[n];
             }
+            
+            PromptDir[filenameLength] = '>';
+            
+            PromptDir[0] = fsGetRootDirectory();
+            PromptDir[1] = '/';
+            
+            ConsoleSetPrompt(PromptDir, filenameLength + 2);
             
         } else {
             
-            // Drop to root directory
+            printc("BBB", 3);
+            printLn();
             
-            directoryStackPtr--;
+            // Drop to the root directory
             
-            fsSetDirectoryStack(directoryStackPtr);
+            fs_directory_stack_ptr = 0;
             
             uint8_t PromptRoot[] = " >";
             
@@ -136,108 +185,6 @@ void functionCD(uint8_t* param, uint8_t param_length) {
             ConsoleSetPrompt(PromptRoot, 3);
             
             fsClearWorkingDirectory();
-            
-        }
-        
-        return;
-    }
-    
-    //
-    // Change to system root
-    //
-    
-    if ((param[0] == '/') & (param[1] == ' ')) {
-        
-        fsSetDirectoryStack(0);
-        
-        uint8_t PromptRoot[] = " >";
-        
-        PromptRoot[0] = fsGetRootDirectory();
-        
-        ConsoleSetPrompt(PromptRoot, 3);
-        
-        fsClearWorkingDirectory();
-        
-        return;
-    }
-    
-    
-    //
-    // Change to a directory
-    //
-    
-    if ((param[0] >= 'a') & (param[0] <= 'z')) {
-        
-        if (fsFileExists(param, param_length-1) != 0) {
-            
-            struct FSAttribute attribute;
-            fsGetFileAttributes(param, param_length-1, &attribute);
-            
-            // Check is a directory
-            if (attribute.type == 'd') {
-                
-                // Check device
-                
-                if (fsCheckDeviceReady() == 0) {
-                    
-                    print(msgDeviceError, sizeof(msgDeviceError));
-                    printLn();
-                    
-                    return;
-                }
-                
-                fsSetWorkingDirectory(param, param_length-1);
-                
-                uint8_t PromptDir[20];
-                
-                if (fsGetRootDirectory() == '/') {
-                    
-                    // Root directory
-                    uint8_t directoryStackPtr = fsGetDirectoryStack() + 1;
-                    fsSetDirectoryStack(directoryStackPtr);
-                    
-                    for (uint8_t n=0; n < 10; n++) 
-                        directoryStack[directoryStackPtr].name[n] = ' ';
-                    
-                    for (uint8_t n=0; n < param_length - 1; n++) {
-                        directoryStack[directoryStackPtr].name[n] = param[n];
-                        
-                        PromptDir[n + 1] = param[n];
-                    }
-                    
-                    PromptDir[param_length] = '>';
-                    
-                    PromptDir[0] = '/';
-                    
-                    ConsoleSetPrompt(PromptDir, param_length + 2);
-                    
-                } else {
-                    
-                    // Device letter
-                    
-                    for (uint8_t i=0; i < param_length + 1; i++) 
-                        PromptDir[i + 2] = param[i];
-                    
-                    PromptDir[param_length + 1] = '>';
-                    
-                    PromptDir[0] = fsGetRootDirectory();
-                    PromptDir[1] = '/';
-                    
-                    ConsoleSetPrompt(PromptDir, param_length + 3);
-                    
-                }
-                
-            } else {
-                
-                print(msgNotDirectory, sizeof(msgNotDirectory));
-                printLn();
-                
-            }
-            
-        } else {
-            
-            print(msgDirectoryNotFound, sizeof(msgDirectoryNotFound));
-            printLn();
             
         }
         
