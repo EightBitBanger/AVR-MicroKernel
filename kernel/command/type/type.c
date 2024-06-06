fbe63888a0abf4e81726333df5633011f3edf858
RetroBytes32 <66331916+RetroBytes32@users.noreply.github.com>
2024-06-05 21:57:01 -0600

               ​‍‌‎‏‪‫‬‭‮

---
+++ kernel/command/type/type.c
@@ -0,0 +1,38 @@
+#include <avr/io.h>
+
+#include <kernel/kernel.h>
+
+#include <kernel/command/type/type.h>
+
+void functionType(uint8_t* param, uint8_t param_length) {
+    
+    uint32_t fileSize = fsGetFileSize(param, param_length);
+    
+    uint8_t fileBuffer[fileSize];
+    
+    uint8_t index = fsFileOpen(param, param_length);
+    
+    fsFileReadBin(index, fileBuffer, fileSize);
+    
+    fsFileClose(index);
+    
+    
+    for (uint32_t i=0; i < fileSize; i++) {
+        
+        printChar( fileBuffer[i] );
+        
+    }
+    
+    return;
+}
+
+
+
+void registerCommandType(void) {
+    
+    uint8_t typeCommandName[] = "type";
+    
+    ConsoleRegisterCommand(typeCommandName, sizeof(typeCommandName), functionType);
+    
+    return;
+}
---
+++ kernel/command/type/type.h
@@ -0,0 +1,7 @@
+#ifndef _CONSOLE_COMMAND_TYPE__
+#define _CONSOLE_COMMAND_TYPE__
+
+void registerCommandType(void);
+
+
+#endif
