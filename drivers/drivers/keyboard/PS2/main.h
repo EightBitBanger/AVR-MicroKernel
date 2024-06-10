5e4a10047655de27314b16cf9e9d2b31284ad45f
RetroBytes32 <66331916+RetroBytes32@users.noreply.github.com>
2024-06-09 19:35:45 -0600

               ​‍‌‎‏‪‫‬‭‮

---
+++ drivers/drivers/keyboard/PS2/main.h
@@ -0,0 +1,28 @@
+#ifndef __PS2_KEYBOARD_DRIVER_
+#define __PS2_KEYBOARD_DRIVER_
+
+#include <avr/io.h>
+
+#include <kernel/bus/bus.h>
+#include <kernel/device/device.h>
+
+uint8_t kbDecodeScanCode(uint8_t scancode_low, uint8_t scancode_high);
+
+void kbPrintInput(void);
+
+struct PS2DeviceDriver {
+    
+    struct Device device;
+    
+    struct Bus interface;
+    
+    uint8_t is_linked;
+    
+    void(*read)(uint32_t address, uint8_t* buffer);
+    void(*write)(uint32_t address, uint8_t buffer);
+    
+};
+
+void initiatePS2Driver(void);
+
+#endif
