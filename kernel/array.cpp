82a3f309750927a045f903feb42e9b9d2b0311b2
EightBitBanger <66331916+EightBitBanger@users.noreply.github.com>
2025-02-01 19:42:33 -0700

    Cache array data to external memory

--- kernel/array.cpp
+++ kernel/array.cpp
@@ -5,278 +5,97 @@ extern "C" {
 #include <kernel/array.hpp>
 }
 
+array::array() : mSize(0), mCapacity(0), mAddress(0), dummyIndex(-1) {}
 
-array::array() : 
-    mSize(0),
-    mCapacity(0),
-    mAddress(0)
-{}
+array::array(uint32_t size) : mSize(size), mCapacity(size), mAddress(new(size)), dummyIndex(-1) {}
 
-array::array(uint32_t size) {
-    
-    mSize = size;
-    mCapacity = size;
-    
-    mAddress = new(mCapacity);
-    
-    return;
-}
-
-array::array(const array& arr) {
-    
-    mSize = arr.mSize;
-    mCapacity = arr.mCapacity;
-    
-    mAddress = new(mCapacity);
-    
+array::array(const array& arr) : mSize(arr.mSize), mCapacity(arr.mCapacity), mAddress(new(arr.mCapacity)), dummyIndex(-1) {
     uint8_t buffer[mSize];
-    
     VirtualRead(arr.mAddress, buffer, mSize);
-    
     VirtualWrite(mAddress, buffer, mSize);
-    
-    return;
 }
 
 array::~array() {
-    
     if (mAddress > 0) 
         delete(mAddress);
-    
-    return;
 }
 
-uint8_t array::operator[](unsigned int const i) {
-    VirtualRead(mAddress, buffer, mSize);
-    
-    return get(i);
-}
+static uint8_t dummy;
 
+uint8_t& array::operator[](unsigned int const i) {
+    if (dummyIndex != i) {
+        updateBuffer(dummyIndex, dummy);
+    }
+    dummyIndex = i;
+    dummy = readBuffer(i);
+    return dummy;
+}
 
 const uint8_t& array::operator[](unsigned int const i) const {
-    
-    buffer
-    VirtualWrite(mAddress, buffer, mSize);
-    
-    VirtualRead(mAddress, buffer, mSize);
-    
-    buffer[i] = mBuffer;
-    
-    VirtualWrite(mAddress, buffer, mSize);
-    
-    return mBuffer;
+    dummy = readBuffer(i);
+    return dummy;
 }
 
-
-void array::set(uint32_t const i, uint8_t item) {
-    
-    uint8_t buffer[mSize];
-    
-    VirtualRead(mAddress, buffer, mSize);
-    
-    buffer[i] = item;
-    
-    VirtualWrite(mAddress, buffer, mSize);
-    
-    return;
+void array::set(uint32_t i, uint8_t item) {
+    if (i < mSize) {
+        updateBuffer(i, item);
+    }
 }
 
-uint8_t array::get(uint32_t const i) {
-    
-    uint8_t buffer[mSize];
-    
-    VirtualRead(mAddress, buffer, mSize);
-    
-    return buffer[i];
+uint8_t array::get(uint32_t i) {
+    if (i < mSize) {
+        return readBuffer(i);
+    }
+    return 0;
 }
 
 bool array::empty(void) {
-    
     return mSize == 0;
 }
 
 uint32_t array::size(void) {
-    
     return mSize;
 }
 
 uint32_t array::max_size(void) {
-    
     return mCapacity;
 }
 
 void array::resize(uint32_t newSize) {
-    
     reserve(newSize);
-    
     mSize = newSize;
-    
-    return;
 }
 
 void array::reserve(uint32_t newCapacity) {
-    
     uint8_t buffer[mCapacity];
-    
     uint32_t newAddress = new(newCapacity);
-    
     VirtualRead(mAddress, buffer, mCapacity);
-    
     VirtualWrite(newAddress, buffer, mCapacity);
-    
     delete(mAddress);
-    
     mAddress = newAddress;
-    
     mCapacity = newCapacity;
-    
-    return;
 }
 
 void array::push_back(uint8_t item) {
-    
     if (mSize >= mCapacity) {
         if (mCapacity == 0) 
             mCapacity = 1;
-        
-        reserve( mCapacity * 2 );
-    }
-    
-    mSize++;
-    
-    set(mSize-1, item);
-    
-    return;
-}
-
-
-
-
-
-/*
-
-
-
-#include <avr/io.h>
-
-extern "C" {
-#include <kernel/kernel.h>
-}
-
-
-template<typename T, uint32_t N>
-array<T, N>::array() : mSize(0), mCapacity(N), mAddress(0) {
-    mAddress = new(mCapacity);
-}
-
-template<typename T, uint32_t N>
-array<T, N>::array(const array& arr) {
-    copy_from(arr);
-}
-
-template<typename T, uint32_t N>
-array<T, N>& array<T, N>::operator=(const array& arr) {
-    if (this != &arr) {
-        delete(mAddress);
-        copy_from(arr);
-    }
-    return *this;
-}
-
-template<typename T, uint32_t N>
-array<T, N>::~array() {
-    if (mAddress > 0) 
-        delete(mAddress);
-}
-
-template<typename T, uint32_t N>
-T& array<T, N>::operator[](unsigned int const i) {
-    static T dummy;
-    VirtualRead(mAddress, mBuffer, sizeof(T));
-    if (i < mSize) {
-        return reinterpret_cast<T&>(mBuffer[i]);
-    }
-    // Return a reference to a dummy variable if out of bounds
-    return dummy;
-}
-
-template<typename T, uint32_t N>
-const T& array<T, N>::operator[](unsigned int const i) const {
-    static T dummy;
-    VirtualRead(mAddress, mBuffer, sizeof(T));
-    if (i < mSize) {
-        return reinterpret_cast<const T&>(mBuffer[i]);
-    }
-    // Return a reference to a dummy variable if out of bounds
-    return dummy;
-}
-
-template<typename T, uint32_t N>
-void array<T, N>::set(unsigned int const i, T item) {
-    uint8_t buffer[sizeof(T)];
-    buffer[i] = reinterpret_cast<uint8_t&>(item);
-    VirtualWrite(mAddress, buffer, sizeof(T));
-}
-
-template<typename T, uint32_t N>
-T array<T, N>::get(unsigned int const i) const {
-    uint8_t buffer[sizeof(T)];
-    VirtualRead(mAddress, buffer, sizeof(T));
-    return reinterpret_cast<T&>(buffer[i]);
-}
-
-template<typename T, uint32_t N>
-uint32_t array<T, N>::size() const {
-    return mSize;
-}
-
-template<typename T, uint32_t N>
-uint32_t array<T, N>::capacity() const {
-    return mCapacity;
-}
-
-template<typename T, uint32_t N>
-void array<T, N>::resize(uint32_t newSize) {
-    reserve(newSize);
-    mSize = newSize;
-}
-
-template<typename T, uint32_t N>
-void array<T, N>::reserve(uint32_t newCapacity) {
-    if (newCapacity <= mCapacity) return;
-    T buffer[mCapacity];
-    uint32_t newAddress = new(newCapacity);
-    VirtualRead(mAddress, buffer, mCapacity);
-    VirtualWrite(newAddress, buffer, mCapacity);
-    delete(mAddress);
-    mAddress = newAddress;
-    mCapacity = newCapacity;
-}
-
-template<typename T, uint32_t N>
-void array<T, N>::push_back(T item) {
-    if (mSize >= mCapacity) {
-        if (mCapacity == 0)
-            mCapacity = 1;
         reserve(mCapacity * 2);
     }
+    updateBuffer(mSize, item);
     mSize++;
-    set(mSize-1, item);
 }
 
-template<typename T, uint32_t N>
-void array<T, N>::copy_from(const array& arr) {
-    mSize = arr.mSize;
-    mCapacity = arr.mCapacity;
-    mAddress = new(mCapacity);
-    T buffer[mSize];
-    VirtualRead(arr.mAddress, buffer, mSize);
+void array::updateBuffer(uint32_t index, uint8_t value) {
+    uint8_t buffer[mSize];
+    VirtualRead(mAddress, buffer, mSize);
+    buffer[index] = value;
     VirtualWrite(mAddress, buffer, mSize);
 }
 
-
-
-*/
-
-
+uint8_t array::readBuffer(uint32_t index) const {
+    uint8_t buffer[mSize];
+    VirtualRead(mAddress, buffer, mSize);
+    return buffer[index];
+}
 
--- kernel/array.hpp
+++ kernel/array.hpp
@@ -1,100 +1,34 @@
 #ifndef __ARRAY_ALLOCATOR_
 #define __ARRAY_ALLOCATOR_
 
-
 class array {
-    
 public:
-    
     array();
     array(uint32_t size);
     array(const array& arr);
     ~array();
     
-    uint8_t operator[](unsigned int const i);
+    uint8_t& operator[](unsigned int const i);
     const uint8_t& operator[](unsigned int const i) const;
     
-    void set(uint32_t const i, uint8_t item);
-    
-    uint8_t get(uint32_t const i);
+    void set(uint32_t i, uint8_t item);
+    uint8_t get(uint32_t i);
     
     bool empty(void);
-    
     uint32_t size(void);
-    
     uint32_t max_size(void);
-    
     void resize(uint32_t newSize);
-    
     void reserve(uint32_t newCapacity);
-    
     void push_back(uint8_t item);
     
 private:
-    
     uint32_t mSize;
     uint32_t mCapacity;
     uint32_t mAddress;
+    uint32_t dummyIndex;
     
-    uint16 mIndexBegin;
-    uint8_t buffer[8];
-    
+    void updateBuffer(uint32_t index, uint8_t value);
+    uint8_t readBuffer(uint32_t index) const;
 };
 
-
 #endif
-
-
-
-
-
-/*
-
-
-
-#ifndef __ARRAY_ALLOCATOR_
-#define __ARRAY_ALLOCATOR_
-
-template<typename T, uint32_t N>
-    
-class array {
-    
-public:
-    
-    array();
-    array(const array& arr);
-    array& operator=(const array& arr);
-    ~array();
-    
-    
-    T& operator[](unsigned int const i); // Get
-    const T& operator[](unsigned int const i) const; // Set
-    
-    void set(unsigned int const i, T item);
-    T get(unsigned int const i) const;
-    
-    uint32_t size() const;
-    uint32_t capacity() const;
-    
-    void resize(uint32_t newSize);
-    void reserve(uint32_t newCapacity);
-    void push_back(T item);
-    
-private:
-    
-    uint32_t mSize;
-    uint32_t mCapacity;
-    uint32_t mAddress;
-    
-    uint8_t mBuffer[sizeof(T)];
-    
-    void copy_from(const array& arr);
-    
-};
-
-#include "array.tpp"
-
-#endif
-
-*/
-
